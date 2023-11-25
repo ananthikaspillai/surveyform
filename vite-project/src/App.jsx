0b8979436a94c9cecb7a90762a22907556083927
ananthikaspillai <ananthika.s@edstem.com>
2023-11-25 06:29:24 +0530

    updated

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,8 +10,7 @@ import Link from './components/Link';
 function App() {
   const [currentForm, setCurrentForm] = useState('login');
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  // const [loginId, setLoginId] = useState(null)
-
+ 
   const toggleForm = (formName) => {
     setCurrentForm(formName);
   }
@@ -61,60 +60,4 @@ export default App;
 
 
 
-// import React, { useState, useEffect } from "react";
-// import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-// import { Login } from "./components/Login";
-// import { Signup } from "./components/Signup";
-// import Home from "./components/Home";
-// import PreviewPage from './components/PreviewPage';
-// import Link from './components/Link';
 
-// function App() {
-//   const [currentForm, setCurrentForm] = useState('login');
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   const toggleForm = (formName) => {
-//     setCurrentForm(formName);
-//   }
-//   const handleLoginSuccess = () => {
-//     setIsLoggedIn(true);
-//   }
-//   const newLoginId = localStorage.getItem('LoginId')
-
-//   useEffect(() => {
-//     if (newLoginId) {
-//       setIsLoggedIn(true)
-//     }
-//     else {
-//       setIsLoggedIn(false)
-//     }
-//   }, [newLoginId])
-
-//   return (
-//     <Router>
-//       <div className="App">
-//         <Routes>
-//           {isLoggedIn ? (
-//             <>
-//             <Route path="/" element={<Home />} />
-//             <Route path="/preview" element={<PreviewPage />} />
-//             <Route path="/Link" element={<Link />} />
-//             </>
-//           ) : (
-//             currentForm === "login" ? (
-//               <>
-//               <Route path="/Link" element={<Navigate to={"/"} />} />
-//               <Route path="/" element={<Login onFormSwitch={toggleForm} onLoginSuccess={handleLoginSuccess} />} />
-//               </>
-//             ) : (
-//               <Route path="/" element={<Signup onFormSwitch={toggleForm} />} />
-//             )
-//           )}
-        
-//         </Routes>
-//       </div>
-//     </Router>
-//   );
-// }
-
-// export default App;
